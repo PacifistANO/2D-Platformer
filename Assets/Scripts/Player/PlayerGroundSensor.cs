138e7c22d0b20cbe2a27679d7f059d6c7b1bf18c
PacifistANO <novartiome@yandex.by>
2024-03-18 23:24:04 +0300

    update

--- Assets/Scripts/Player/PlayerGroundSensor.cs
+++ Assets/Scripts/Player/PlayerGroundSensor.cs
@@ -5,12 +5,13 @@ public class PlayerGroundSensor : MonoBehaviour
     [SerializeField] private LayerMask _layerMask;
 
     private bool _isCollided;
+    private float _checkDistance = 0.1f;
 
     public bool IsCollided => _isCollided;
 
     private void FixedUpdate()
     {
-        if (Physics2D.Raycast(transform.position, -transform.up, 0.1f, _layerMask))
+        if (Physics2D.Raycast(transform.position, -transform.up, _checkDistance, _layerMask))
         {
             _isCollided = true;
         }
